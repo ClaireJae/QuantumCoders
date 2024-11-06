71a3a8d494e81e8ff78f4efd4d4f19827ba14c3d
Justin Gwinn <gwinnj3@gmail.com>
2024-11-06 14:48:30 -0600

    added a form and some javascript code

---
+++ JS/justinPage2.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form")
+const textField = document.querySelector("#text-choice")
+const output93 = document.querySelector("#output93")
+const output94 = document.querySelector("#output94")
+const output95 = document.querySelector("#output95")
+const output96 = document.querySelector("#output96")
+const output97 = document.querySelector("#output97")
+const output98 = document.querySelector("#output98")
+const output99 = document.querySelector("#output99")
+const output2000 = document.querySelector("#output2000")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const choice = textField.value
+    switch(choice){
+        
+    }
+})
