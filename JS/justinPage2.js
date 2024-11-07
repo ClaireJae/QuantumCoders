458c20c30e367fb6d48592cb05c15b910eca7bf7
Justin Gwinn <gwinnj3@gmail.com>
2024-11-07 10:04:13 -0600

    add some javascript

--- JS/justinPage2.js
+++ JS/justinPage2.js
@@ -11,8 +11,54 @@ const output2000 = document.querySelector("#output2000")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const choice = textField.value
-    switch(choice){
-        case 1994: 
+    const choice = textField.value;
+    
+    console.log(textField.value)
+    switch (true) {
+        case choice == 1994: output94.append(createYearElement(choice))
+            
+            break;
+    
+        default:
+            break;
     }
-})
+     
+    
+    
+});
+
+const loadYearsFromJson = async () => {
+    const response = await fetch("../JSONData/data1993to2000.json")
+    const data = await response.json();
+    return await data;
+};
+
+const createYearElement = async (time) => {
+    const data = await loadYearsFromJson();
+    var yearData = new Year;
+    console.log(data)
+    data.forEach((object) => {
+        if(object.year === time){
+            yearData = new Year(data.year, data.paragraph1, data.paragraph2);
+            console.log(yearData)
+
+            const yearElement = document.createElement("div");
+            yearElement.classList.add("created-year");
+            yearElement.innerHTML = `
+                <p>${yearData.paragraph1}</p>
+            `
+            return yearElement
+        }
+    })
+
+    
+    
+}
+
+class Year{
+    constructor(year, paragraph1, paragraph2){
+        this.year = year;
+        this.paragraph1 = paragraph1;
+        this.paragraph2 = paragraph2;
+    }
+};
