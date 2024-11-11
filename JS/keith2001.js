c3163ae77ecfa0c26d915807a4a898ca4f6da0d6
Claire Jaeger <claire_jaeger@stu.indianhills.edu>
2024-11-11 13:25:25 -0600

    Text changes in JS

--- JS/keith2001.js
+++ JS/keith2001.js
@@ -5,10 +5,10 @@ const prevButton = document.querySelector("#prevButton")
 const nextButton = document.querySelector("#nextButton")
 
 const texts = [
-    "This is when the Cha Cha Slide was made.",
+    "This is when the Cha Cha Slide was made!",
     "Slide to the left",
     "Slide to the right!---------->",
-    "Okay enough"
+    "Awoo!"
 ]
 
 function updateText() {
